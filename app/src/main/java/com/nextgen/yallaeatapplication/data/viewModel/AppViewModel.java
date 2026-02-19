f1a3986c7f75db05f893647fa5448396567151f4
khaderAbuShaban <kabushaban2@smail.ucas.edu.ps>
2026-02-19 09:41:20 +0200

    add all user-related database functionality

---
+++ app/src/main/java/com/nextgen/yallaeatapplication/data/viewModel/AppViewModel.java
@@ -0,0 +1,55 @@
+package com.nextgen.yallaeatapplication.data.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.nextgen.yallaeatapplication.data.model.User;
+import com.nextgen.yallaeatapplication.data.repository.AppRepository;
+
+
+public class AppViewModel extends AndroidViewModel {
+
+    private AppRepository repository;
+    private MutableLiveData<User> currentUser = new MutableLiveData<>();
+
+    public AppViewModel(@NonNull Application application) {
+        super(application);
+        repository = new AppRepository(application);
+    }
+    // ----------------- USERS -----------------
+
+    public void setCurrentUser(User user) {
+        currentUser.setValue(user);
+    }
+
+    public LiveData<User> getCurrentUser() {
+        return currentUser;
+    }
+
+    public LiveData<User> loginUser(String username, String password) {
+        return repository.loginUser(username, password);
+    }
+
+    public void registerUser(User user) {
+        repository.insertUser(user);
+    }
+
+    public void updateUser(User user) {
+        repository.updateUser(user);
+        currentUser.postValue(user);
+    }
+
+    public LiveData<User> getUserByUsername(String username) {
+        LiveData<User> userLiveData = repository.getUserByUsername(username);
+        userLiveData.observeForever(user -> {
+            if (user != null) {
+                currentUser.postValue(user);
+            }
+        });
+        return userLiveData;
+    }
+}
