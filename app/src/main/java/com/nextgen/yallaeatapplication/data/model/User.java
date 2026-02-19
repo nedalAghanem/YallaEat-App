f3ce681db57270fd1df033eefcc1ac43cb229fc8
RaghebAbuShaban <rabushaban2004@smail.ucas.edu.ps>
2026-02-19 09:17:34 +0200

    setup RoomDatabase with User entity and UserDao

---
+++ app/src/main/java/com/nextgen/yallaeatapplication/data/model/User.java
@@ -0,0 +1,33 @@
+package com.nextgen.yallaeatapplication.data.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String username;
+    private String password;
+    private String type;
+
+    public User(String username, String password, String type) {
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+}
