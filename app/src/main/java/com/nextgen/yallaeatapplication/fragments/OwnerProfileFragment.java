47299ed1e23798e5d8b68c973a6e77d95cca2d66
Mohammed Moqaiad <ma293@smail.ucas.edu.ps>
2026-03-07 06:51:34 +0200

    Added CustomerMainActivity, OwnerMainActivity, and OwnerMenuFragment

---
+++ app/src/main/java/com/nextgen/yallaeatapplication/fragments/OwnerProfileFragment.java
@@ -0,0 +1,45 @@
+package com.nextgen.yallaeatapplication.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nextgen.yallaeatapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OwnerProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OwnerProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_owner_profile, container, false);
+
+        TextView tvUser = v.findViewById(R.id.tvUsername);
+        Button btnLogout = v.findViewById(R.id.btnLogout);
+
+        SharedPreferences prefs = getActivity().getSharedPreferences("app_prefs", getContext().MODE_PRIVATE);
+        String username = prefs.getString("username", "Owner");
+        tvUser.setText("Welcome, " + username);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.clear();
+                editor.apply();
+
+                startActivity(new Intent(getActivity(), AuthActivity.class));
+                getActivity().finish();
+            }
+        });
+
+        return v;
+    }
+}
